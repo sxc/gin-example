103ce01c6eb32f9a40390a4dad465e922a592689
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-04-19 20:08:55 +0800

    add a name handler

--- main.go
+++ main.go
@@ -6,9 +6,16 @@ import (
 
 func main() {
 	router := gin.Default()
+	router.GET("/:name", IndexHandler)
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello World"})
 	})
 	router.Run(":8080")
+}
+
+func IndexHandler(c *gin.Context) {
+	name := c.Param("name")
+	c.JSON(200, gin.H{
+		"message": "Hello " + name})
 }
